2ea1ef8e6e4fa665b2765018bb17f918f2c72871
ChWitt <christoph.witting@googlemail.com>
2014-05-13 11:29:32 +0200

    Startup

---
+++ CustomUpdater/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomUpdater
+{
+    public class Class1
+    {
+    }
+}
